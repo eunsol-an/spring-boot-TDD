e8fe2724aba45201071869e8e893c6fcf847a13d
eunsol-an <dev.eunsol@gmail.com>
2025-07-09 22:39:51 +0900

    feat: UserPoint 도메인에 포인트 충전 및 사용 로직 추가

--- src/main/java/io/hhplus/tdd/point/UserPoint.java
+++ src/main/java/io/hhplus/tdd/point/UserPoint.java
@@ -6,7 +6,32 @@ public record UserPoint(
         long updateMillis
 ) {
 
+    private static final long MAX_BALANCE = 100_000;
+
     public static UserPoint empty(long id) {
         return new UserPoint(id, 0, System.currentTimeMillis());
+    }
+
+    public UserPoint charge(long amount) {
+        if (amount <= 0) {
+            throw new IllegalArgumentException("포인트 충전은 0보다 큰 금액만 가능합니다");
+        }
+
+        long newBalance = point + amount;
+        if (newBalance > MAX_BALANCE) {
+            throw new IllegalArgumentException("포인트 잔고는 100,000원을 초과할 수 없습니다");
+        }
+
+        return new UserPoint(id, point + amount, System.currentTimeMillis());
+    }
+
+    public UserPoint use(long amount) {
+        if (amount <= 0) {
+            throw new IllegalArgumentException("포인트 사용은 0보다 큰 금액만 가능합니다.");
+        }
+        if (point < amount) {
+            throw new IllegalArgumentException("잔액이 부족합니다.");
+        }
+        return new UserPoint(id, point - amount, System.currentTimeMillis());
     }
 }
